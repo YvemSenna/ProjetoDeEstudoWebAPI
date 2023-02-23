ea592afe26e711392b060a388ae498b66d915837
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-23 16:32:16 -0300

    ImplementandoMigration

---
+++ SistemaDeTarefas/SistemaDeTarefas/Enums/StatusTarefa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description("A fazer")]
+        AFazer = 1,
+        [Description("Em Andamenti")]
+        EmAndamento = 2,
+        [Description("Concluido")]
+        Concluido = 3,
+    }
+}
