54d516365bb4b9aeb1e562899e72422a328bae57
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-23 15:47:34 -0300

    ConfigurandoClasseProgram

--- SistemaDeTarefas/SistemaDeTarefas/Repositorios/Interfaces/IUsuarioRepositorio.cs
+++ SistemaDeTarefas/SistemaDeTarefas/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -8,5 +8,7 @@ namespace SistemaDeTarefas.Repositorios.Interfaces
         Task<List<UsuarioModel>> BuscarTodosUsuarios();
         Task<UsuarioModel> BuscarPorId(int id);
         Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
+        Task<bool> Apagar(int id);
     }
 }
