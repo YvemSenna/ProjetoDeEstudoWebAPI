b59a0f92d31cb31659f79b7e092aecdb02d7adcd
Yvem Senna <t_jefersons@hapvida.com.br>
2023-02-23 14:15:06 -0300

    CriandoPrimeiroEndPoint

---
+++ SistemaDeTarefas/SistemaDeTarefas/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult <List<UsuarioController>> BuscarTodosUsuario()
+        {
+            return Ok();
+        }
+    }
+}
